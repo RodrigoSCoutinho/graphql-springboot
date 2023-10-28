35e84a5ff333f03af6d39aa571edd21d8f8e8745
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-28 16:18:11 -0300

    first commit

---
+++ src/main/java/br/com/rodrigoscoutinho/graphqlspringboot/GraphqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoscoutinho.graphqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootApplication.class, args);
+	}
+
+}
