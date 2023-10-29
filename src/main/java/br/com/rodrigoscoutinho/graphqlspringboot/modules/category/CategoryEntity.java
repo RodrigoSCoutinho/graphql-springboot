f8df9c559746fcd416f7792b7de98a22736c5f79
Rodrigo Coutinho <rodrigos.coding@gmail.com>
2023-10-28 23:25:40 -0300

    feature: adding products and fix the bugs

--- src/main/java/br/com/rodrigoscoutinho/graphqlspringboot/modules/category/CategoryEntity.java
+++ src/main/java/br/com/rodrigoscoutinho/graphqlspringboot/modules/category/CategoryEntity.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CategoryEntity {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
